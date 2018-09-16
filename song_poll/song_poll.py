8c3303e8a8ab30487ecda339085467b8bde7bfe7
Drew Warren <drewbwarren@gmail.com>
2018-09-15 22:26:51 -0700

    Select two random songs

--- song_poll/song_poll.py
+++ song_poll/song_poll.py
@@ -3,9 +3,14 @@
 import sys
 import spotipy
 import spotipy.util as util
+import random
+
+
+## Take the tracks data from the playlist
 
 scope = 'user-library-read'
 username = '1295552060'
+playlist = '0UGlsRHlwdrsnYcgty9dCR'
 
 token = util.prompt_for_user_token(username, scope)
 
@@ -19,11 +24,37 @@ artists = []
 
 i = 0
 while(True):
-    results = sp.user_playlist_tracks(username, playlist_id='0UGlsRHlwdrsnYcgty9dCR', offset=i, limit=1)
-    results = results['items'][0]['track']
-    tracks.append(results['name'])
-    artists.append(results['artists'][0]['name'])
-    i+=1
+    results = sp.user_playlist_tracks(username, playlist, offset=i, limit=100)
+    results = results['items']
+
+    for track in results:
+        track = track['track']
+        tracks.append(track['name'])
+        artists.append(track['artists'][0]['name'])
+
+    i+=100
+    if len(results) < 100:
+        break
 
 
-print(tracks)
+
+## Start the voting loop
+N = len(tracks)
+while(True):
+
+    while(True):
+        ind1 = random.randint(0,len(tracks)-1)
+        ind2 = random.randint(0,len(tracks)-1)
+        if ind1 != ind2:
+            break
+
+    song1 = tracks[ind1]
+    song2 = tracks[ind2]
+    artist1 = artists[ind1]
+    artist2 = artists[ind2]
+
+    print(song1 + " --- " + artist1)
+    print(song2 + " --- " + artist2)
+    raw_input()
+
+
