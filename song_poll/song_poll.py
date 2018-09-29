bb198f787b954f96d938c7b4c1b9962c1560746a
Drew Warren <drewbwarren@gmail.com>
2018-09-29 00:51:38 -0700

    Play buttons not supported
    Apparently the SDK doesn't let you start playback on individual songs
    unless you're on an Android or iOS platform.

--- song_poll/song_poll.py
+++ song_poll/song_poll.py
@@ -6,28 +6,26 @@ import spotipy.util as util
 import random
 import Tkinter as tk
 
-## Button commands for ui
-def show_button():
-    print(song1)
-
 
 ## Take the tracks data from the playlist
 
-scope = 'user-library-read'
+# My personal Spotify info
+scope = 'user-library-read streaming'
 username = '1295552060'
 playlist = '0UGlsRHlwdrsnYcgty9dCR'
 
+# Authorization to use the Spotify SDK
 token = util.prompt_for_user_token(username, scope)
-
 if not token:
     sys.exit()
-
 sp = spotipy.Spotify(auth=token)
 
+# Initialize lists of information to be used
 tracks = []
 artists = []
 track_ids = []
 
+# Extract the track info from the playlist. Each item is in a very specific position of the data structure returned by the sp.user_playlist_tracks() function
 i = 0
 while(True):
     results = sp.user_playlist_tracks(username, playlist, offset=i, limit=100)
@@ -39,6 +37,7 @@ while(True):
         artists.append(track['artists'][0]['name'])
         track_ids.append(track['uri'])
 
+    # The function can only return 100 tracks at a time.
     i+=100
     if len(results) < 100:
         break
@@ -47,15 +46,18 @@ while(True):
 root = tk.Tk()
 root.title('Song Poll')
 label = tk.Label(root, text="Choose one: ", padx=100)
+label.grid(row=0, column=1)
 
 ## Start the voting loop
 def voting_loop(button1, button2):
+    # Select two random and unique tracks from the playlist
     while(True):
         ind1 = random.randint(0,len(tracks)-1)
         ind2 = random.randint(0,len(tracks)-1)
         if ind1 != ind2:
             break
 
+    # Collect the info about the tracks
     song1 = tracks[ind1]
     song2 = tracks[ind2]
     artist1 = artists[ind1]
@@ -65,21 +67,39 @@ def voting_loop(button1, button2):
     id1 = track_ids[ind1]
     id2 = track_ids[ind2]
 
+    # Change the button text and pass the track info into the button_command
     button1.config(text=entry1, command=lambda: button_command(song1, artist1, id1))
     button2.config(text=entry2, command=lambda: button_command(song2, artist2, id2))
-    button1.pack()
-    button2.pack()
+    play_button1.config(command=lambda: play_command(id1))
+    play_button2.config(command=lambda: play_command(id2))
 
+# When the button is pressed, this function is called
 def button_command(song, artist, song_id):
     print(song, artist, song_id)
     voting_loop(button1, button2)
 
+# Define the function to play the songs using the play buttons
+def play_command(song_id):
+    print(song_id)
+    print(song_id[0])
+    sp.start_playback(context_uri=song_id)
 
-N = len(tracks)
+# Initialize the two song buttons
 button1 = tk.Button(root, width=50, command=button_command)
+button1.grid(row=1, column=0)
 button2 = tk.Button(root, width=50, command=button_command)
+button2.grid(row=2, column=0)
+
+# Initialize the two play buttons
+play_button1 = tk.Button(root, width=5, text='>')
+play_button1.grid(row=1, column=2)
+play_button2 = tk.Button(root, width=5, text='>')
+play_button2.grid(row=2, column=2)
+
+# Begin the voting
 voting_loop(button1, button2)
 
-tk.Button(root, text='Quit', command=root.quit).pack()
+# Create a button for quitting the program
+tk.Button(root, text='Quit', command=root.quit).grid(row=3, column=1)
 tk.mainloop()
 
