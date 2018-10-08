3e84b8be5e44eaf195d0c429d8037e57d882445b
Drew Warren <drewbwarren@gmail.com>
2018-10-07 23:11:33 -0700

    Parse data from results file

--- song_poll/song_poll.py
+++ song_poll/song_poll.py
@@ -7,6 +7,7 @@ import random
 import Tkinter as tk
 import json
 from subprocess import Popen
+import time
 
 
 ## Open Spotify so the songs can play
@@ -29,23 +30,49 @@ sp = spotipy.Spotify(auth=token)
 tracks = []
 artists = []
 track_ids = []
+score = [] # how many times thesong has been chosen
+at_bat = [] # how many times the song has been available to vote on
 
-# Extract the track info from the playlist. Each item is in a very specific position of the data structure returned by the sp.user_playlist_tracks() function
-i = 0
-while(True):
-    results = sp.user_playlist_tracks(username, playlist, offset=i, limit=100)
-    results = results['items']
+# Extract the track info from the playlist. Each item is in a very specific position of ...
+# the data structure returned by the sp.user_playlist_tracks() function
+# Only run this if there is not already a results file available
+try:
+    # If there is already a score file, grab the data
+    read_file = open('poll_results.json', 'r')
+    data = json.load(read_file)
+    for row in data:
+        tracks.append(row[0])
+        artists.append(row[1])
+        track_ids.append(row[2])
+        score.append(row[3])
+        at_bat.append(row[4])
+    print('file found')
+except IOError:
+    # If there is no score file, extract the tracks from spotify
+    i = 0
+    while(True):
+        results = sp.user_playlist_tracks(username, playlist, offset=i, limit=100)
+        results = results['items']
 
-    for track in results:
-        track = track['track']
-        tracks.append(track['name'])
-        artists.append(track['artists'][0]['name'])
-        track_ids.append(track['uri'])
+        for track in results:
+            track = track['track']
+            tracks.append(track['name'])
+            artists.append(track['artists'][0]['name'])
+            track_ids.append(track['uri'])
 
-    # The function can only return 100 tracks at a time.
-    i+=100
-    if len(results) < 100:
-        break
+        # The function can only return 100 tracks at a time.
+        i+=100
+        if len(results) < 100:
+            break
+    score = [0]*len(tracks)
+    at_bat = [0]*len(tracks)
+    print('file not found')
+
+# Start playing the playlist to activate the device, then immediately puase it
+# time.sleep(10)
+# context = "spotify:playlist:" + playlist
+# sp.start_playback(context_uri=context)
+# sp.pause_playback()
 
 ## Set up the ui
 root = tk.Tk()
@@ -72,24 +99,25 @@ def voting_loop(button1, button2):
     id1 = track_ids[ind1]
     id2 = track_ids[ind2]
 
+    # Update the at_bat list to show these two songs have been selected
+    at_bat[ind1] += 1
+    at_bat[ind2] += 1
+
     # Change the button text and pass the track info into the button_command
-    button1.config(text=entry1, command=lambda: button_command(song1, artist1, id1))
-    button2.config(text=entry2, command=lambda: button_command(song2, artist2, id2))
+    button1.config(text=entry1, command=lambda: button_command(ind1))
+    button2.config(text=entry2, command=lambda: button_command(ind2))
     play_button1.config(command=lambda: play_command(id1))
     play_button2.config(command=lambda: play_command(id2))
 
-# When the button is pressed, this function is called
-def button_command(song, artist, song_id):
-    print(song, artist, song_id)
+# When the button is pressed, the score for the winning song increments
+def button_command(ind):
+    global score
+    score[ind] += 1
     voting_loop(button1, button2)
 
 # Define the function to play the songs using the play buttons
 def play_command(song_id):
-    print(song_id)
-    # new_id_list = track_ids[0:10]
-    # new_id_list[0] = song_id
     new_id_list = [song_id]
-    print(new_id_list)
     sp.start_playback(uris=new_id_list)
 
 # Initialize the two song buttons
